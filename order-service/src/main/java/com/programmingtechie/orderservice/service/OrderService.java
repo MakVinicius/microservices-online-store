c7c8ed0527d15c0349922c181fe73fc956f312db
MakVinicius <mvgj369@gmail.com>
2023-11-13 07:03:32 -0300

    feat: Adding Order microservice starting files

---
+++ order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
@@ -0,0 +1,42 @@
+package com.programmingtechie.orderservice.service;
+
+import com.programmingtechie.orderservice.dto.OrderLineItemsDTO;
+import com.programmingtechie.orderservice.dto.OrderRequest;
+import com.programmingtechie.orderservice.model.Order;
+import com.programmingtechie.orderservice.model.OrderLineItems;
+import com.programmingtechie.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDTOList()
+                .stream()
+                .map(this::mapToDTO)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDTO(OrderLineItemsDTO orderLineItemsDTO) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDTO.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDTO.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDTO.getSkuCode());
+
+        return orderLineItems;
+    }
+}
